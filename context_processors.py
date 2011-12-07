12a5cbd34868ab9392711db8f03dd642751f5f24
Anthony Nemitz <anemitz@gmail.com>
2011-12-07 10:56:38 -0800

    hope this works

--- context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.sites.models import Site
-from django.conf import settings
-
-def globals(request):
-    return {
-        'site': Site.objects.get_current(),
-        'ssl': request.is_secure(),
-    } 
